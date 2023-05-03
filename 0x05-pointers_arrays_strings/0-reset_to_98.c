97745bf6643f0e9c6d3afb0b847fed4c1900d5d5
Fortune Ellah <ellfortune3@gmail.com>
2023-05-03 16:51:06 -0700

    updates

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
  * reset_to_98 - function that takes a poniter to an int
  * as parameter
  * and updates the value to 98
- * @n: input
- * Returns: n
+ * @n: the value to be updated
+ * Returns: always 0
  */
 void reset_to_98(int *n)
 {
