cc1ef5056625d038aff3ddc3feb40e41fb92a53b
Fortune Ellah <ellfortune3@gmail.com>
2023-05-17 04:55:40 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ int count_word(char *s)
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[c] == "")
+		if (s[c] == '')
 			flag = 0;
 		else if (flag == 0)
 		{
@@ -51,7 +51,7 @@ char **strtow(char *str)
 
 	for (i = 0; i <= len; i++)
 	{
-		if (str[i] == "" || str[i] == '\0')
+		if (str[i] == '' || str[i] == '\0')
 		{
 			if (c)
 			{
