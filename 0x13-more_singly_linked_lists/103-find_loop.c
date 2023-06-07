542a6dea73e7e9d04be1ed1e752e931975544472
Fortune Ellah <ellfortune3@gmail.com>
2023-06-06 19:48:08 -0700

    new

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -20,7 +20,7 @@ listint_t *find_listint_loop(listint_t *head)
 		slow = slow->next;
 		if (fast == slow)
 		{
-			slow = he`ad;
+			slow = head;
 			while (slow != fast)
 			{
 				slow = slow->next;
